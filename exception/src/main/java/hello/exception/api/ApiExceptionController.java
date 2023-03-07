df75af0f3dd0a286a08e4c49bab33412e49fe841
mothership <mothership2002@naver.com>
2023-03-07 16:14:06 +0900

    ResponseStatusException, defaultHandlerException

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,13 +1,16 @@
 package hello.exception.api;
 
+import hello.exception.web.exception.BadRequestException;
 import hello.exception.web.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -27,6 +30,21 @@ public class ApiExceptionController {
             throw new UserException();
         }
         return new MemberDto( id, "hello " + id );
+    }
+
+    @GetMapping( "/api/response-status-ex1" )
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping( "/api/response-status-ex2" )
+    public String responseStatusEx2() {
+        throw new ResponseStatusException( HttpStatus.NOT_FOUND, "error.404", new IllegalArgumentException() );
+    }
+
+    @GetMapping( "/api/default-handler-ex" )
+    public String defaultException( @RequestParam Integer data ) {
+        return "ok";
     }
 
     @Data
