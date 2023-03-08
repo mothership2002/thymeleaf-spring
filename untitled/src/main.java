a7a690af22db592ee362c3f74ec7fe3d8cf1259e
mothership <mothership2002@naver.com>
2023-03-08 22:27:41 +0900

    lambda practice

--- untitled/src/main.java
+++ untitled/src/main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
 //        Temp2 temp1 = new Temp2() {
 //            @Override
 //            public void temp2() {
@@ -19,12 +20,23 @@ public class main {
 
     public static void temp() {
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(String.valueOf(i));
-        }
-        System.out.println(list.stream().filter(e -> Integer.parseInt(e) == 3)
+//        for (int i = 0; i < 10; i++) {
+//            list.add(String.valueOf(i));
+//        }
+        LongStream.range( 1l, ( long ) Math.ceil( ( Math.random() * 100 ) ) )
+                .mapToObj( Long :: valueOf )
+                .forEach( e -> {
+                    list.add( String.valueOf( e ) );
+                    System.out.println(e);
+                } );
+
+        System.out.println( "==========================" );
+        list.forEach( e -> System.out.printf("%s ", e) );
+
+        System.out.println("\n" +  list.stream().filter( e -> Integer.parseInt( e ) == 3 )
                 .findFirst()
-                .orElse(null));
+                .orElse( null ) );
 
     }
+
 }
